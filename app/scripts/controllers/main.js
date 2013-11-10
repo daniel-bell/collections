f7641dda5d77bcaa333c0be0739d26f957f56ac3
Daniel Bell <me@daniel-bell.co.uk>
2013-11-10 14:26:20 +0000

    Can now remove collections
    Can now remove collection items
    
    New list is now created when trying to add an item to a null list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,6 @@ angular.module('collectionsApp')
 	function init(){
 		$scope.collections = [];
 		$scope.collections = collectionFactory.getCollections();
-		$scope.currentCollection = null;
 	}
 
 	$scope.addCollection = function(){
@@ -22,7 +21,43 @@ angular.module('collectionsApp')
 	}
 
 	$scope.addItem = function(){
-		$scope.currentCollection.items.push($scope.newItem);
-		$scope.newItem = "";
+		if($scope.currentCollection != null){
+			$scope.currentCollection.items.push($scope.newItem);
+			$scope.newItem = "";
+		}
+		else{
+			// Add a new collection
+			var num = 0;
+			var tempListName = "";
+			var found = false;
+
+			while(!found){
+				num++;
+				tempListName = "New List " + num;
+
+				for(var i = 0; i < $scope.collections.length; i++){
+					if(!($scope.collections[i].name == tempListName)){
+						found = true;
+					}
+				}
+			}
+			$scope.collections.push({name: tempListName, items: []})
+			$scope.currentCollection = $scope.collections[$scope.collections.length-1];
+			$scope.currentCollection.items.push($scope.newItem);
+		}
+	}
+
+	$scope.deleteCollection = function(collection){
+		var index = $scope.collections.indexOf(collection);
+		$scope.collections.splice(index, 1);
+
+		if($scope.currentCollection == collection){
+			$scope.currentCollection = null;
+		}
+	}
+
+	$scope.deleteItem = function(item){
+		var index = $scope.currentCollection.items.indexOf(item);
+		$scope.currentCollection.items.splice(index, 1);
 	}
  });
